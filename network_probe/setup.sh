40e55386862659e8878b192d5f0196ff532193fe
Yevhen Khmelevskyi (Raspberry Pi 4 host) <khmellevskyi.ye23@gmail.com>
2025-02-02 17:22:52 +0100

    updated network_probe setup.sh file

--- network_probe/setup.sh
+++ network_probe/setup.sh
@@ -3,18 +3,21 @@
 echo "Deploying configuration files..."
 
 # Copy Telegraf configuration
-sudo cp network_probe/telegraf/telegraf.conf /etc/telegraf/telegraf.conf
+#sudo cp network_probe/telegraf/telegraf.conf /etc/telegraf/telegraf.conf
 
 # Copy Suricata configuration
+sudo suricata-update
+sudo cp network_probe/suricata/rules/custom.rules /etc/suricata/rules/custom.rules
 sudo cp network_probe/suricata/suricata.yaml /etc/suricata/suricata.yaml
+sudo suricata -T -c /etc/suricata/suricata.yaml
 
 # Copy nProbe configuration
-sudo cp network_probe/nprobe/nprobe.conf /etc/nprobe/nprobe.conf
+#sudo cp network_probe/nprobe/nprobe.conf /etc/nprobe/nprobe.conf
 
 # Restart services to apply changes
 echo "Restarting services..."
-sudo systemctl restart telegraf
+#sudo systemctl restart telegraf
 sudo systemctl restart suricata
-sudo systemctl restart nprobe
+#sudo systemctl restart nprobe
 
 echo "Deployment complete!"
